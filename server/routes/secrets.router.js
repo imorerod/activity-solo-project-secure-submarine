fbfa012376fdf06848c658369d4d09027d7b5887
Isaac Morerod <isaacmorerod@isaacsMacBookPro.local>
2019-05-30 13:22:33 -0500

    secret results based on clearance level

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -3,16 +3,18 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('req.user:', req.user);
-    if(req.user.clearance_level >= 13) {
-        pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
-        });
-    } else {
-        res.sendStatus(403);
+    if(req.isAuthenticated()) {
+        console.log('req.user:', req.user);
+        let queryString = `SELECT * FROM "secret" WHERE "secrecy_level" <= $1;`;
+        pool.query(queryString,[req.user.clearance_level])
+            .then(results => res.send(results.rows))
+            .catch(error => {
+                console.log('Error making SELECT for secrets:', error);
+                res.sendStatus(500);
+            });
+    }
+    else {
+        res.sendStatus(403)
     }
 });
 
